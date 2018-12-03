e75ad95aa03f6712b27ffe92ed029dff14f0be05
thomasfouan <thomasfouan@hotmail.fr>
2018-12-03 15:02:02 +0100

    finish chouette combination calculation. Replace junit 4 with junit 5 for parameterized test

--- src/main/java/com/kaamelott/CombinationCalculator.java
+++ src/main/java/com/kaamelott/CombinationCalculator.java
@@ -2,6 +2,20 @@ package com.kaamelott;
 
 class CombinationCalculator {
     int computeScore(String roll) {
-        return 1;
+        for (int i = 1; i < 7; i++) {
+            if (has2DicesOf(roll, i)) {
+                return i*i;
+            }
+        }
+        return 0;
     }
+
+    private boolean has2DicesOf(String roll, int digit) {
+        long nbDicesOfDigit = roll
+                .chars()
+                .filter(dice -> dice == Character.forDigit(digit, 10))
+                .count();
+        return nbDicesOfDigit == 2;
+    }
+
 }
--- src/test/java/com/kaamelott/CombinationCalculatorTest.java
+++ src/test/java/com/kaamelott/CombinationCalculatorTest.java
@@ -1,17 +1,24 @@
 package com.kaamelott;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CombinationCalculatorTest {
+class CombinationCalculatorTest {
 
-    @Test
-    public void should_compute_chouette_combination_calculation() {
-        String roll = "113";
-
+    @ParameterizedTest(name = "Chouette de {index} ({0}) : {1}")
+    @CsvSource(value = {
+            "113, 1",
+            "242, 4",
+            "433, 9",
+            "446, 16",
+            "515, 25",
+            "566, 36",
+    })
+    void should_compute_chouette_combination(String roll, int expectedResult) {
         int score = new CombinationCalculator().computeScore(roll);
 
-        assertThat(score).isEqualTo(1);
+        assertThat(score).isEqualTo(expectedResult);
     }
 }
