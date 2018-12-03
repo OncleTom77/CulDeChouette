fc25b140cc257e3c4168b3d9fd9a7c3784b2770d
thomasfouan <thomasfouan@hotmail.fr>
2018-12-03 15:40:39 +0100

    add velute combination and remove special case of chouette in test argument

--- src/main/java/com/kaamelott/CombinationCalculator.java
+++ src/main/java/com/kaamelott/CombinationCalculator.java
@@ -1,13 +1,49 @@
 package com.kaamelott;
 
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+
 class CombinationCalculator {
     int computeScore(String roll) {
+        Optional<Integer> hasVelute = hasVelute(roll);
+        if (hasVelute.isPresent()) {
+            return hasVelute.get();
+        }
+
+        Optional<Integer> hasChouette = hasChouette(roll);
+        if (hasChouette.isPresent()) {
+            return hasChouette.get();
+        }
+
+        return 0;
+    }
+
+    private Optional<Integer> hasVelute(String roll) {
+        int firstDigit = Character.digit(roll.charAt(0), 10);
+        int secondDigit = Character.digit(roll.charAt(1), 10);
+        int thirdDigit = Character.digit(roll.charAt(2), 10);
+
+        List<Integer> digits = asList(firstDigit, secondDigit, thirdDigit);
+        digits.sort(Integer::compareTo);
+
+        Optional<Integer> hasVelute = Optional.empty();
+        if (digits.get(0) + digits.get(1) == digits.get(2)) {
+            hasVelute = Optional.of(digits.get(2) * digits.get(2) * 2);
+        }
+
+        return hasVelute;
+    }
+
+    private Optional<Integer> hasChouette(String roll) {
+        Optional<Integer> hasChouette = Optional.empty();
         for (int i = 1; i < 7; i++) {
             if (has2DicesOf(roll, i)) {
-                return i*i;
+                hasChouette = Optional.of(i*i);
             }
         }
-        return 0;
+        return hasChouette;
     }
 
     private boolean has2DicesOf(String roll, int digit) {
--- src/test/java/com/kaamelott/CombinationCalculatorTest.java
+++ src/test/java/com/kaamelott/CombinationCalculatorTest.java
@@ -10,15 +10,30 @@ class CombinationCalculatorTest {
     @ParameterizedTest(name = "Chouette de {index} ({0}) : {1}")
     @CsvSource(value = {
             "113, 1",
-            "242, 4",
             "433, 9",
             "446, 16",
             "515, 25",
             "566, 36",
     })
-    void should_compute_chouette_combination(String roll, int expectedResult) {
+    void should_compute_chouette_combination_as_the_power_of_2_of_the_number_on_the_identical_dices(String roll, int expectedScore) {
         int score = new CombinationCalculator().computeScore(roll);
 
-        assertThat(score).isEqualTo(expectedResult);
+        assertThat(score).isEqualTo(expectedScore);
+    }
+
+
+    @ParameterizedTest(name = "Velute ({0}) : {1}")
+    @CsvSource(value = {
+            "123, 18",
+            "413, 32",
+            "154, 50",
+            "235, 50",
+            "156, 72",
+            "246, 72",
+    })
+    void should_compute_velute_combination_as_the_double_of_the_power_of_2_of_max_number(String roll, int expectedScore) {
+        int score = new CombinationCalculator().computeScore(roll);
+
+        assertThat(score).isEqualTo(expectedScore);
     }
 }
